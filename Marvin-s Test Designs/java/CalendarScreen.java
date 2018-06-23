2f5c8984d7c165c5aaa360c27f520980a11c504a
marvinabum1998 <33125471+marvinabum1998@users.noreply.github.com>
2018-06-23 17:32:31 +0100

    Add files via upload

--- Marvin-s Test Designs/java/CalendarScreen.java
+++ Marvin-s Test Designs/java/CalendarScreen.java
@@ -1,13 +1,58 @@
 package com.marvinabum.tflagenda;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
+        import java.util.Locale;
 
 public class CalendarScreen extends AppCompatActivity {
+
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_screen);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //Set an event as an example
+
+        Event ev1 = new Event(Color.RED, 1477040400000L, "Example");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+
+                if (dateClicked.toString().compareTo("Fri Oct 21 00:00:00 AST 2016") == 0) {
+                    Toast.makeText(context, "Example", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No events planned for that day", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
     }
 }
